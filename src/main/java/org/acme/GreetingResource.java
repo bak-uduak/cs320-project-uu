c482554340aa32e4529f0304bed104c88a44c43f
bak-uduak <uuduaks@ensign.edu>
2024-10-06 01:34:44 -0600

    1.4 Final Update

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -22,4 +22,19 @@ public class GreetingResource {
     public String personalizedHello(@PathParam("name") String name) { 
         return "Hello " + name; 
     }
+    @POST
+    @Path("/personalized")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String personalizedHelloPost(Person p) {
+        return "Hello" + p.getFirst() + " " + p.getLast();
+    }
+    public static class  Person {
+        private String first;
+        private String last;
+
+        public String getFirst() { return first; }
+        public void setFirst(String first) { this.first = first; }
+        public String getLast() { return last; }
+        public void setLast(String last) { this.last = last; }
+    }
 }
